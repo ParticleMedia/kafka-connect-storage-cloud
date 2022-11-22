b2dabe8528f163ceeea9a10917056b07e7f7b287
tonglin-newsbreak <tong.lin@newsbreak.com>
2022-11-22 11:09:01 +0800

    update

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/TopicPartitionWriter.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/TopicPartitionWriter.java
@@ -308,8 +308,9 @@ public class TopicPartitionWriter {
               currentOffset
       );
       currentSchemas.put(encodedPartition, valueSchema);
-      nextState();
-      return true;
+// 注释掉，永远不返回true
+//      nextState();
+//      return true;
     }
 
     SinkRecord projectedRecord = compatibility.project(record, null, currentValueSchema);
